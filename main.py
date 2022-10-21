dd85083e32123cd3c45fd0fb71ea8b687f060f04
Mixail <maximov.maksmisha@yandex.ru>
2022-10-21 17:47:09 +0300

    add file

---
+++ main.py
@@ -0,0 +1,11 @@
+v = float(input("Введите скорость ракеты \n"))
+if (v <= 7.8) and (v >0) :
+    print(0)
+elif (v > 7.8) and (v <= 11.2):
+    print(1)
+elif (v >= 11.2) and (v<=16.4):
+    print(2)
+elif (v > 16.4):
+    print(3)
+else:
+    print("error")
